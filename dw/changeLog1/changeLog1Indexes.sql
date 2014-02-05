439ba17247cf010bf70e80d5b9fd070ca85529a8
Dave Steinich <drsteini@usgs.gov>
2014-02-05 19:46:46 +0000

    WQP-265 Improve WQP database query responses

--- dw/changeLog1/changeLog1Indexes.sql
+++ dw/changeLog1/changeLog1Indexes.sql
@@ -11,26 +11,14 @@ create bitmap index fa_station_primary_type_00000 on fa_station_00000 (primary_s
 --rollback drop index fa_station_primary_type_00000;
 
 --changeset drsteini:1SchemaIndexesAC
-drop index nwis_station_sum_4_00000;
---rollback create bitmap index nwis_station_sum_4_00000 on nwis_station_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)) nologging;
-
---changeset drsteini:1SchemaIndexesAD
 create bitmap index nwis_station_sum_4_00000 on nwis_station_sum_00000 (primary_site_type) nologging;
 --rollback drop index nwis_station_sum_4_00000;
 
---changeset drsteini:0SchemaIndexesAE
-drop index nwis_result_sum_3_00000;
---rollback create bitmap index nwis_result_sum_3_00000 on nwis_result_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)   ) local nologging;
-
---changeset drsteini:0SchemaIndexesAF
+--changeset drsteini:0SchemaIndexesAD
 create bitmap index nwis_result_sum_3_00000 on nwis_result_sum_00000 (primary_site_type) local nologging;
 --rollback drop index nwis_result_sum_3_00000;
 
---changeset drsteini:0SchemaIndexesAG
-drop index nwis_result_ct_sum_3_00000;
---rollback create bitmap index nwis_result_ct_sum_3_00000 on nwis_result_ct_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)   ) local nologging;
-
---changeset drsteini:0SchemaIndexesAH
+--changeset drsteini:0SchemaIndexesAE
 create bitmap index nwis_result_ct_sum_3_00000 on nwis_result_ct_sum_00000 (primary_site_type) local nologging;
 --rollback drop index nwis_result_ct_sum_3_00000;
 
--- dw/changeLog1/changeLog1Tables.sql
+++ dw/changeLog1/changeLog1Tables.sql
@@ -3,29 +3,44 @@
 --This is for the nwis_ws_star schema
  
 --changeset drsteini:1SchemaTablesAA
-alter table fa_station_00000 add (primary_site_type varchar2(40 byte));
+alter table fa_station_00000 add (primary_site_type varchar2(40 char));
 --rollback alter table fa_station_00000 drop column primary_site_type;
+
+
+--changeset drsteini:1SchemaTableABIndexes
+drop index nwis_station_sum_4_00000;
+--rollback create bitmap index nwis_station_sum_4_00000 on nwis_station_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)) nologging;
 
 --changeset drsteini:1SchemaTablesAB
 alter table nwis_station_sum_00000 rename column station_type_name to primary_site_type;
 --rollback alter table nwis_station_sum_00000 rename column primary_site_type to station_type_name;
 
 --changeset drsteini:1SchemaTablesAC
-alter table nwis_station_sum_00000 modify(primary_site_type varchar2(40 byte));
+alter table nwis_station_sum_00000 modify(primary_site_type varchar2(40 char));
 --rollback alter table nwis_station_sum_00000 modify(primary_site_type varchar2(82 byte));
+
+
+--changeset drsteini:0SchemaTablesADIndexes
+drop index nwis_result_sum_3_00000;
+--rollback create bitmap index nwis_result_sum_3_00000 on nwis_result_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)   ) local nologging;
 
 --changeset drsteini:1SchemaTablesAD
 alter table nwis_result_sum_00000 rename column station_type_name to primary_site_type;
 --rollback alter table nwis_result_sum_00000 rename column primary_site_type to station_type_name;
 
 --changeset drsteini:1SchemaTablesAE
-alter table nwis_result_sum_00000 modify(primary_site_type varchar2(40 byte));
+alter table nwis_result_sum_00000 modify(primary_site_type varchar2(40 char));
 --rollback alter table nwis_result_sum_00000 modify(primary_site_type varchar2(82 byte));
+
+
+--changeset drsteini:0SchemaTablesAFIndexes
+drop index nwis_result_ct_sum_3_00000;
+--rollback create bitmap index nwis_result_ct_sum_3_00000 on nwis_result_ct_sum_00000 (substr(station_type_name || ':', 1, instr(station_type_name || ':', ':') - 1)   ) local nologging;
 
 --changeset drsteini:1SchemaTablesAF
 alter table nwis_result_ct_sum_00000 rename column station_type_name to primary_site_type;
 --rollback alter table nwis_result_ct_sum_00000 rename column primary_site_type to station_type_name;
 
 --changeset drsteini:1SchemaTablesAG
-alter table nwis_result_ct_sum_00000 modify(primary_site_type varchar2(40 byte));
+alter table nwis_result_ct_sum_00000 modify(primary_site_type varchar2(40 char));
 --rollback alter table nwis_result_ct_sum_00000 modify(primary_site_type varchar2(82 byte));
