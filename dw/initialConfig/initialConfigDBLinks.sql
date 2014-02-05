e061c1346957a7f518c6b601ef2a1fb08f28d047
Dave Steinich <drsteini@usgs.gov>
2014-02-05 19:33:23 +0000

    WQP-77 - create liquibase scripts

--- dw/initialConfig/initialConfigDBLinks.sql
+++ dw/initialConfig/initialConfigDBLinks.sql
@@ -2,14 +2,14 @@
 
 --This is for the nwis_ws_star schema
  
---changeset drsteini:1SchemaDBLinksAA
+--changeset drsteini:0SchemaDBLinksAA
 create database link nwq_stg_dbstage.er.usgs.gov connect to nwq_stg identified by "${nwq_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwq_stg_dbstage.er.usgs.gov;
 
---changeset drsteini:1SchemaDBLinksAB
+--changeset drsteini:0SchemaDBLinksAB
 create database link nwis_ws_stg_dbstage.er.usgs.gov connect to nwq_stg identified by "${nwis_ws_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwis_ws_stg_dbstage.er.usgs.gov;
 
---changeset drsteini:1SchemaDBLinksAC
+--changeset drsteini:0SchemaDBLinksAC
 create database link storetmodern_dbdw.er.usgs.gov connect to storetmodern identified by "${storetmodern_pwd}" using 'dbdw.er.usgs.gov';
 --rollback drop database link storetmodern_dbdw.er.usgs.gov;
