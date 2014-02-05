4f5d9400115b62df51bc8bb0b2842a3d19f6eb7d
Dave Steinich <drsteini@usgs.gov>
2014-02-05 19:18:41 +0000

    quote the passwords

--- dw/initialConfig/initialConfigDBLinks.sql
+++ dw/initialConfig/initialConfigDBLinks.sql
@@ -3,9 +3,9 @@
 --This is for the nwis_ws_star schema
  
 --changeset drsteini:1SchemaDBLinksAA
-create database link nwq_stg_dbstage.er.usgs.gov connect to nwq_stg identified by ${nwq_stg_pwd} using 'dbstage.er.usgs.gov';
+create database link nwq_stg_dbstage.er.usgs.gov connect to nwq_stg identified by "${nwq_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwq_stg_dbstage.er.usgs.gov;
 
 --changeset drsteini:1SchemaDBLinksAB
-create database link nwis_ws_stg_dbstage.er.usgs.gov connect to nwq_stg identified by ${nwis_ws_stg_pwd} using 'dbstage.er.usgs.gov';
+create database link nwis_ws_stg_dbstage.er.usgs.gov connect to nwq_stg identified by "${nwis_ws_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwq_stg_dbstage.er.usgs.gov;
