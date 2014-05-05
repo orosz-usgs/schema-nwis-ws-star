6a2e8ebf96186701c5d85e022fdc15d2df82735b
Steinich, Dave R. <drsteini@usgs.gov>
2014-05-05 09:20:20 -0500

    cleanup dos characters

--- dw/ciData/data/county.sql
+++ dw/ciData/data/county.sql
@@ -7,3 +7,71 @@ insert into county_00000 (code_value, description, country_cd, state_cd, sort_or
  values ('US:19:083', 'US, IOWA, HARDIN', 'US', '19', 3);
 insert into county_00000 (code_value, description, country_cd, state_cd, sort_order)
  values ('US:19:169', 'US, IOWA, STORY', 'US', '19', 4);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:000', 'US, WISCONSIN, UNSPECIFIED', 'US', '55', 3058);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:001', 'US, WISCONSIN, ADAMS', 'US', '55', 3059);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:003', 'US, WISCONSIN, ASHLAND', 'US', '55', 3060);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:005', 'US, WISCONSIN, BARRON', 'US', '55', 3061);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:007', 'US, WISCONSIN, BAYFIELD', 'US', '55', 3062);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:009', 'US, WISCONSIN, BROWN', 'US', '55', 3063);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:011', 'US, WISCONSIN, BUFFALO', 'US', '55', 3064);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:013', 'US, WISCONSIN, BURNETT', 'US', '55', 3065);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:015', 'US, WISCONSIN, CALUMET', 'US', '55', 3066);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:017', 'US, WISCONSIN, CHIPPEWA', 'US', '55', 3067);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:019', 'US, WISCONSIN, CLARK', 'US', '55', 3068);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:021', 'US, WISCONSIN, COLUMBIA', 'US', '55', 3069);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:023', 'US, WISCONSIN, CRAWFORD', 'US', '55', 3070);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:025', 'US, WISCONSIN, DANE', 'US', '55', 3071);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:027', 'US, WISCONSIN, DODGE', 'US', '55', 3072);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:029', 'US, WISCONSIN, DOOR', 'US', '55', 3073);
+Insert into county
+   (CODE_VALUE, DESCRIPTION, COUNTRY_CD, STATE_CD, SORT_ORDER)
+ Values
+   ('US:55:031', 'US, WISCONSIN, DOUGLAS', 'US', '55', 3074);
