70c808fceb987b5014adc4f2215c4d270404d000
David Steinich <drsteini@usgs.gov>
2015-04-06 15:54:28 -0500

    WQP-633

--- changeLog1/dbLinks.sql
+++ changeLog1/dbLinks.sql
@@ -2,14 +2,14 @@
 
 --This is for the nwis_ws_star schema
  
---changeset drsteini:0SchemaDBLinksAA
+--changeset drsteini:1DBLinksAA
 create database link nwq_stg.er.usgs.gov connect to nwq_stg identified by "${nwq_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwq_stg.er.usgs.gov;
 
---changeset drsteini:0SchemaDBLinksAB
+--changeset drsteini:1DBLinksAB
 create database link nwis_ws_stg.er.usgs.gov connect to nwis_ws_stg identified by "${nwis_ws_stg_pwd}" using 'dbstage.er.usgs.gov';
 --rollback drop database link nwis_ws_stg.er.usgs.gov;
 
---changeset drsteini:0SchemaDBLinksAC
-create database link storetmodern.er.usgs.gov connect to storetmodern identified by "${storetmodern_pwd}" using 'dbdw.er.usgs.gov';
---rollback drop database link storetmodern.er.usgs.gov;
+--changeset drsteini:1DBLinksAB
+create database link nemi.er.usgs.gov connect to nemi_data identified by "${nemi_data_pwd}" using 'dbtrans.er.usgs.gov';
+--rollback drop database link nemi.er.usgs.gov;
