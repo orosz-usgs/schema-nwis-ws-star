3cb9dd9447d29daa7e89813a4d92b4fb5e0776ff
Mary Bucknell <mbucknell@usgs.gov>
2018-11-21 11:29:31 -0600

    Added wqp specific postgres preload set up

--- liquibase/dbInit/1_run_preload_liquibase.sh
+++ liquibase/dbInit/1_run_preload_liquibase.sh
@@ -15,3 +15,18 @@ ${LIQUIBASE_HOME}/liquibase \
 	-DNWIS_WS_STAR_OWNER=${NWIS_WS_STAR_OWNER} \
 	-DNWIS_WS_STAR_OWNER_PASSWORD=${NWIS_WS_STAR_OWNER_PASSWORD} \
 	 > $LIQUIBASE_HOME/logs/liquibasePostgres.log
+
+# postgres to pubs db scripts
+${LIQUIBASE_HOME}/liquibase \
+	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
+	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/wqp/changeLog.yml \
+	--driver=org.postgresql.Driver \
+	--url=jdbc:postgresql://NWIS_WS_Star_Database:5432/${NWIS_WS_STAR_DATABASE_NAME} \
+	--username=postgres \
+	--password=${POSTGRES_PASSWORD} \
+	--logLevel=debug \
+	update \
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+	-DNWIS_WS_STAR_OWNER=${NWIS_WS_STAR_OWNER} \
+	-DNWIS_WS_STAR_SCHEMA_NAME=${NWIS_WS_STAR_SCHEMA_NAME} \
+	 > $LIQUIBASE_HOME/logs/liquibaseWQP.log
