6eeaf3fbac8f2618eb085967d1a02911714d6df1
Mary Bucknell <mbucknell@usgs.gov>
2019-03-18 16:01:26 -0500

    Change flag fields in site_tp to booleans

--- liquibase/changeLogs/wqp/preload/tables/siteTp/siteTp.sql
+++ liquibase/changeLogs/wqp/preload/tables/siteTp/siteTp.sql
@@ -1,8 +1,8 @@
 create unlogged table if not exists ${NWIS_WS_STAR_SCHEMA_NAME}.site_tp
 (site_tp_cd                     character varying (7)
 ,site_tp_srt_nu                 smallint
-,site_tp_vld_fg                 character varying (1)
-,site_tp_prim_fg                character varying (1)
+,site_tp_vld_fg                 boolean
+,site_tp_prim_fg                boolean
 ,site_tp_nm                     character varying (10)
 ,site_tp_ln                     character varying (40)
 ,site_tp_ds                     text
