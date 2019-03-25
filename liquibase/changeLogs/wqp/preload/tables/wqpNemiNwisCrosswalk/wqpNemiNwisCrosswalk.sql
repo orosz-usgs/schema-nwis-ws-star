a7e0e8b1bb3e911e515ad53752d11ca9d3039f09
Mary Bucknell <mbucknell@usgs.gov>
2019-03-25 12:32:42 -0500

    method_id field needs to be bigint

--- liquibase/changeLogs/wqp/preload/tables/wqpNemiNwisCrosswalk/wqpNemiNwisCrosswalk.sql
+++ liquibase/changeLogs/wqp/preload/tables/wqpNemiNwisCrosswalk/wqpNemiNwisCrosswalk.sql
@@ -2,7 +2,7 @@ create unlogged table if not exists ${NWIS_WS_STAR_SCHEMA_NAME}.wqp_nemi_nwis_cr
 (analytical_procedure_source    character varying (100)
 ,analytical_procedure_id        character varying (100)
 ,citation_method_num            character varying (100)
-,method_id                      integer
+,method_id                      bigint
 ,wqp_source                     character varying (100)
 ,method_type                    character varying (100)
 ,nemi_url                       text
