156b2a1b41e599fdbdfa50b3656798e90725a17c
Mary Bucknell <mbucknell@usgs.gov>
2019-04-09 15:45:34 -0500

    The create view syntax does not include "if not exists"

--- liquibase/changeLogs/nwis/views/fullState/fullState.sql
+++ liquibase/changeLogs/nwis/views/fullState/fullState.sql
@@ -1,4 +1,4 @@
-create view if not exists ${NWIS_SCHEMA_NAME}.full_state (state_code, state_name) as
+create view ${NWIS_SCHEMA_NAME}.full_state (state_code, state_name) as
 select country_cd || ':' || state_cd,
        state_nm
   from ${NWIS_SCHEMA_NAME}.state;
