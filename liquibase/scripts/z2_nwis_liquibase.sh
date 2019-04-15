44eefd0e7bc44f9c5f0cd0a0037b5647fed031a7
David Steinich <drsteini@contractor.usgs.gov>
2019-04-15 11:59:53 -0500

    testing etl db

--- liquibase/scripts/z2_nwis_liquibase.sh
+++ liquibase/scripts/z2_nwis_liquibase.sh
@@ -3,7 +3,7 @@
 # nwis schema scripts
 ${LIQUIBASE_HOME}/liquibase \
 	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
-	--changeLogFile=${LIQUIBASE_WORKSPACE}/nwis/changeLog.yml \
+	--changeLogFile=${LIQUIBASE_WORKSPACE_NWIS}/nwis/changeLog.yml \
 	--driver=org.postgresql.Driver \
 	--url=jdbc:postgresql://${NWIS_DATABASE_ADDRESS}:5432/${NWIS_DATABASE_NAME} \
 	--username=${NWIS_SCHEMA_OWNER_USERNAME} \
