1d16723b70db95f61cd6144a5e171c52b5739074
David Steinich <drsteini@contractor.usgs.gov>
2019-11-04 15:34:14 -0600

    IOW-108 - Adjust field sizes

--- liquibase/changeLogs/nwis/tables/huc/huc.sql
+++ liquibase/changeLogs/nwis/tables/huc/huc.sql
@@ -1,5 +1,5 @@
 create unlogged table if not exists ${NWIS_SCHEMA_NAME}.huc
-(code                           character varying (5)
+(code                           character varying (16)
 ,name                           character varying (64)
 ,class_code                     character varying (1)
 ,primary key (code)
