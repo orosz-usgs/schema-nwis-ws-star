a80f0dd0c7a75a4b9206e5dbce89568b5db0bc66
Alice McCarthy <ajmccart@usgs.gov>
2019-12-10 15:06:23 -0600

    Rename network and network sites folders and files to use camelcase rather than underscores.  Rename network_site_rel to network_sites and network_site_rel_staging to network_sites_staging

--- liquibase/changeLogs/nwis/tables/networkSitesStaging/networkSitesStaging.sql
+++ liquibase/changeLogs/nwis/tables/networkSitesStaging/networkSitesStaging.sql
@@ -1,4 +1,4 @@
-create table if not exists ${NWIS_SCHEMA_NAME}.network_site_rel_staging
+create table if not exists ${NWIS_SCHEMA_NAME}.network_sites_staging
 (net_code                                character varying (15)
 ,site_no                                 character varying (30)
 ,net_name                                character varying (255)
