66525310324cb56c4a03c33b3ddc9c781bc7b1d1
Alice McCarthy <ajmccart@usgs.gov>
2019-12-17 13:31:02 -0600

    Remove unique key on network.network_name

--- liquibase/changeLogs/nwis/tables/network/network.sql
+++ liquibase/changeLogs/nwis/tables/network/network.sql
@@ -4,5 +4,4 @@ create table if not exists ${NWIS_SCHEMA_NAME}.network
 ,network_name                            character varying (255)
 ,primary key (network_id)
 ,constraint network_uk1 unique (network_code)
-,constraint network_uk2 unique (network_name)
 );
