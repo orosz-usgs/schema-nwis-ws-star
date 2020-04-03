928b8e68c50a6b57809d60b46d7318bac59a06ae
David Steinich <drsteini@contractor.usgs.gov>
2020-04-03 09:06:43 -0500

    IOW-156 - from natdb changes

---
+++ liquibase/changeLogs/nwis/tables/gwLevelMethod/gwLevelMethod.sql
@@ -0,0 +1,8 @@
+create unlogged table if not exists ${NWIS_SCHEMA_NAME}.gw_level_method
+(code                           character varying (25)
+,name                           character varying (16)
+,sort_order                     integer
+,description                    character varying (255)
+,valid_flag                     boolean
+,primary key (code)
+) with (fillfactor = 100);
